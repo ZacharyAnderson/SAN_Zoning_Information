d872fe9610dc0e2b4dd1dad58a1929504541a863
Zach Anderson <zacha777@gmail.com>
2018-03-28 13:57:05 -0400

    Added zoneDB function as well as query function. Also added api route for querying zone db from front end.

--- back-end/brocade_functions.py
+++ back-end/brocade_functions.py
@@ -7,23 +7,54 @@ and import them to global variables to be used throughout this
 file.
 '''
 def main():
-    FABRIC_KEY = {}
+    #Global Variables are initiated and defined
     config = configparser.ConfigParser()
     config.read('config.ini')
     BROCADE_USERNAME = config['BROCADE']['CMCNE_USERNAME']
     BROCADE_PASSWORD = config['BROCADE']['CMCNE_PASSWORD']
-    FABRIC_KEY[0] = config['BROCADE']['LYNDHURST_REDFABRIC']
-    FABRIC_KEY[1] = config['BROCADE']['LYNDHURST_BLUEFABRIC']
-
+    FABRIC_KEY = [config['BROCADE']['LYNDHURST_REDFABRIC'], config['BROCADE']['LYNDHURST_BLUEFABRIC']]
     base_url = 'https://vsesgplconman/rest/'
+   
+    zoneDB = createZoneDB(FABRIC_KEY, base_url, brocadeAPILogin(BROCADE_USERNAME, BROCADE_PASSWORD, base_url))
+    queryZoneDB('pipsweb4n', zoneDB)
 
+
+def createZoneDB(FABRIC_KEY, base_url, WStoken):
+    '''
+        This function will be used to log in to all Fabrics in the organization
+        and create a zoneDB that will be able to to show the data of all active zones
+    '''
+    zoneDB = {}
+    for key in FABRIC_KEY:
+        url = base_url + 'resourcegroups/All/fcfabrics/' + key + '/zones/'
+        headers = {'WStoken':WStoken, 'Accept':'application/vnd.brocade.networkadvisor+json;version=v1'}
+        req = requests.get(url = url, headers = headers, verify = False)
+        zone_info = json.loads(req.text)
+        for member in zone_info['zones']:
+            zoneDB[member['name'].lower()] = member['memberNames']
+    return (zoneDB)
+
+
+def brocadeAPILogin(BROCADE_USERNAME, BROCADE_PASSWORD, base_url):
+    '''
+        This function is used to log into the brocade API and returns the WStoken 
+        that is needed for all calls afterwards.
+
+    '''
     request = requests.post(url = base_url + 'login', headers = {'WSUsername':BROCADE_USERNAME, 'WSPassword':BROCADE_PASSWORD}, verify = False)
-    print (request.headers['WStoken'])
+    return (request.headers['WStoken'])
 
-    req = requests.get(url = base_url + 'resourcegroups/All/fcfabrics/' + FABRIC_KEY[0] + '/zones/', headers = {'WStoken':request.headers['WStoken'], 'Accept':'application/vnd.brocade.networkadvisor+json;version=v1'}, verify = False)
-    zone_info = json.loads(req.text)
-    pprint(zone_info)
-    
+def queryZoneDB(hostname, zoneDB):
+    '''
+        queryZoneDB will take an input of a hostname and output the zones associated with the name
+        if no zones are associated it will also let you know that the hostname has no zones associated.
+        It will output the zones in JSON format.
+    '''
+    found_hosts = {}
+    for key in zoneDB.keys():
+        if hostname.lower() in key:
+            found_hosts[key] = zoneDB[key]
+    return found_hosts
 
 
 if __name__ == '__main__':
--- back-end/server.py
+++ back-end/server.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import configparser, urllib3
+import brocade_functions
 
 '''
 This is where we will set up all api calls to output 
 requested information from reactJS front-end
 '''
-
-
+#Global Variables are initiated and defined
+config = configparser.ConfigParser()
+config.read('config.ini')
+BROCADE_USERNAME = config['BROCADE']['CMCNE_USERNAME']
+BROCADE_PASSWORD = config['BROCADE']['CMCNE_PASSWORD']
+FABRIC_KEY = [config['BROCADE']['LYNDHURST_REDFABRIC'], config['BROCADE']['LYNDHURST_BLUEFABRIC']]
+base_url = 'https://vsesgplconman/rest/'
 app = Flask(__name__)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#Creating the first zoneDB
+zoneDB = brocade_functions.createZoneDB(FABRIC_KEY, base_url, brocade_functions.brocadeAPILogin(BROCADE_USERNAME, BROCADE_PASSWORD, base_url))
 
-@app.route('/hostname/')
-def get_hostname_info():
-    return "hostname_info"
+@app.route('/')
+def backend_splashscreen():
+    return ('Welcome to the back-end splash screen. \n You should not be here.')
+
+@app.route('/saninfo/api/v1.0/hostname/<host>', methods =['GET'])
+def get_hostname_info(host):
+    return jsonify(brocade_functions.queryZoneDB(host, zoneDB))
 
 if __name__ == "__main__":
     app.run()
